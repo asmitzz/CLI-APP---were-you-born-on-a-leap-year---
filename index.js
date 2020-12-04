ef4d665fb105cf38732764e1e8db39f43cf04864
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-04 17:58:33 +0530

    change code

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ while(loop){
   var date = parseInt(DOB.slice(0,2));
   var mon = parseInt(DOB.slice(3,5));
    
-   if( date.toString().length <= 2 && mon.toString().length <= 2 && year.toString().length === 4 ){
-       if(!isNaN(date) && !isNaN(mon) && !isNaN(year)){
-          loop = false;
+   if( date <= 31 && mon <= 12 && year.toString().length === 4) {
+           loop = false;
            if( year % 100 === 0 ){
                  if( year % 400 === 0 ){
                  console.log(chalk.green('\nCongratulations, You are born on a leap year'));
@@ -25,22 +24,33 @@ while(loop){
                  }
             }
            else{
-             if( year % 4 === 0 ){
-             console.log(chalk.green('\nCongratulations, You are born on a leap year'));
-             console.log(chalk.blue('Share this on social media'));
+               if( year % 4 === 0 ){
+               console.log(chalk.green('\nCongratulations, You  are born on a leap year'));
+               console.log(chalk.blue('Share this on social media'));
              }
              else{
-            console.log(chalk.red('\nSorry')+chalk.red(',You are not born on a leap year'));
+              console.log(chalk.red('\nSorry')+chalk.red(',You are not born on a leap year'));
             }
+         }
+   }
+   else{
+       if( (date > 31 && date.toString().length <= 2 )){
+          console.log(chalk.red('Invalid date ! please input valid date'));
        }
-   }
-      else{
-       console.log(chalk.red('Wrong input format!! Please enter a right DOB format'));
-      }
-   }
-  else{
-       console.log(chalk.red('Wrong input format!! Please enter a DOB in this ( DD/MM/YYYY ) format'));
-  }
-   
-
+       else if( (mon > 12 && mon.toString().length <=2 )  ){
+          console.log(chalk.red('Invalid month ! please input valid month'));
+       }
+       else if(isNaN(date)){
+          console.log(chalk.red('Invalid date ! please input valid date'));
+       }
+       else if(isNaN(mon)){
+          console.log(chalk.red('Invalid month ! please input valid month'));
+       }
+       else if(isNaN(year)){
+          console.log(chalk.red('Invalid year ! please input valid year'));
+       }
+       else{
+         console.log(chalk.red('Invalid DOB format ! please input DOB in this format ( DD/MM/YYYY )'))
+       }
+    }
 }
